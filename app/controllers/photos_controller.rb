d0291af022ec1ecd8944f3a01124fb99c842f0f2
duytsev <duytsev@gmail.com>
2016-08-19 01:22:50 +0300

    blank search fixed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,9 @@
 class PhotosController < ApplicationController
 
   def index
-    if params[:search]
+    if params[:search].blank?
+      @photos = Photo.paginate(page: params[:page]).order('id ASC')
+    else
       params[:search].split.each do |tag|
         found_photos = Photo.where(['photos.id IN (SELECT photo_id FROM taggings INNER JOIN tags ON taggings.tag_id = tags.id WHERE tags.content = ?)', tag])
         if @photos
@@ -11,8 +13,6 @@ class PhotosController < ApplicationController
         end
       end
       @photos = @photos.paginate(page: params[:page])
-    else
-      @photos = Photo.paginate(page: params[:page]).order('id ASC')
     end
   end
 
